7cf14696563208f794473b3f6929f2fb8c79704c
Akhmed Musaurov <MUSAUROV-A@MAiL.RU>
2022-10-08 14:07:48 +0300

    1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Write("Введите трехзначное число ");
+int num = int.Parse(Console.ReadLine());
+if (num > 99 & num < 1000)
+{
+    int a = num/10%10;
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("ошибка!!");
+}
